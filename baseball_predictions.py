6598abe8347456fbf97e04bde9f6969b509bdb87
Devin Dhaliwal <devindha@usc.edu>
2020-04-18 23:21:59 -0700

    added more stats to make model more accurate

--- baseball_predictions.py
+++ baseball_predictions.py
@@ -8,14 +8,22 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from matplotlib import style
 import pickle
+pd.options.mode.chained_assignment = None
 
-#getting hitting data
+#getting hitting avgs data
 data = pd.read_csv("baseball.csv")
 data = data.filter(items=['Team','Year','W','OBP','SLG','BA'])
 
-#getting pitching data
+#adding OPS
+OPS_data = data[['OBP','SLG']]
+sum_col = OPS_data['OBP'] + OPS_data['SLG']
+OPS_data['OPS'] = sum_col
+OPS_data = OPS_data.drop(['OBP','SLG'],1)
+data = pd.merge(data, OPS_data, left_index=True,right_index=True)
+
+#getting other hitting and pitching data
 data_1 = pd.read_csv("teams.csv")
-data_1 = data_1.filter(items=['teamID','yearID','W','ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP'])
+data_1 = data_1.filter(items=['teamID','yearID','R','H','2B','3B','HR','BB','SO','SB','CS','W','RA','ER','ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP'])
 data_1 = data_1.query('yearID >= 1962')
 data_1 = data_1.rename(columns={'teamID':'Team','yearID':'Year'})
 
@@ -24,54 +32,18 @@ data_comb = pd.merge(data,data_1,on=['Team','Year'])
 data_comb = data_comb.rename(columns={'W_x':'W'})
 
 #dropping uneeded data after combining
-#data = data.drop(['Team','Year'],1)
-#data_1 = data_1.drop(['Team','Year'],1)
 data_comb = data_comb.drop(['Team','Year','W_y'],1)
 
 #normalizing features
 scalar = MinMaxScaler()
-#data_scaled = scalar.fit_transform(data)
-#data1_scaled = scalar.fit_transform(data_1)
 data_comb_scaled = scalar.fit_transform(data_comb.drop(['W'],1))
 
 #putting data back into dataframe
-#data_scaled_df = pd.DataFrame(data=data_scaled, columns=['W','OBP','SLG','BA'])
-#data1_scaled_df = pd.DataFrame(data=data1_scaled, columns=['W','ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP'])
-data_comb_scaled_df = pd.DataFrame(data=data_comb_scaled, columns=['OBP','SLG','BA','ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP'])
+data_comb_scaled_df = pd.DataFrame(data=data_comb_scaled, columns=['OBP','SLG','BA','OPS','R','H','2B','3B','HR','BB','SO','SB','CS','RA','ER','ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP'])
 
 #adding non normalized wins back to dataframe
 data_wins = data_comb[['W']]
 data_comb_df = pd.merge(data_wins, data_comb_scaled_df, left_index=True,right_index=True)
-
-
-"""
-#using linear regression
-linear = linear_model.LinearRegression()
-
-#getting attribute and label arrays for hitting
-attr = np.array(data_scaled_df.drop(['W'],1))
-labl = np.array(data_scaled_df['W'])
-
-#training model
-attr_train, attr_test, labl_train, labl_test = sklearn.model_selection.train_test_split(attr,labl, test_size=0.2)
-linear.fit(attr_train, labl_train)
-
-#accuracy of model using only hitting data
-acc = linear.score(attr_test, labl_test)
-print("HITTING STATS:",acc*100, "% accuracy")
-
-#getting attribute and label arrays for pitching
-attr1 = np.array(data1_scaled_df.drop(['W'],1))
-labl1 = np.array(data1_scaled_df['W'])
-
-#training model
-attr1_train, attr1_test, labl1_train, labl1_test = sklearn.model_selection.train_test_split(attr1,labl1, test_size=0.2)
-linear.fit(attr1_train, labl1_train)
-
-#accuracy of model for only pitching data
-acc1 = linear.score(attr1_test, labl1_test)
-print("PITCHING STATS:",acc1*100, "% accuracy")
-"""
 
 #getting attribute and label arrays for combined data
 attr_comb = np.array(data_comb_df.drop(['W'],1))
@@ -80,10 +52,11 @@ labl_comb = np.array(data_comb_df['W'])
 #splitting data into training and test
 attr_comb_train, attr_comb_test, labl_comb_train, labl_comb_test = sklearn.model_selection.train_test_split(attr_comb,labl_comb, test_size=0.2)
 
+#training model to be saved
 """
 best_acc = 0
 #saving best model out of 100 attempts
-for i in range(100):
+for i in range(1000):
 
     #splitting data into training and test
     attr_comb_train, attr_comb_test, labl_comb_train, labl_comb_test = sklearn.model_selection.train_test_split(attr_comb,labl_comb, test_size=0.2)
@@ -120,7 +93,6 @@ acc_comb = linear.score(attr_comb_test, labl_comb_test)
 print("\nACCURACY:",acc_comb*100, "PERCENT")
 
 #printing menu
-#stats = ['OBP', 'SLG', 'BA', 'ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP']
 print("STAT OPTIONS:")
 for stat in data_comb:
     if stat != 'W':
@@ -135,17 +107,11 @@ while 1:
         print("Invalid Stat, Please Try Again.")
 
 style.use("ggplot")
-plt.scatter(data_comb[x_attr],data_comb_df['W'])
+plt.scatter(data_comb[x_attr],data_comb['W'])
 plt.xlabel(x_attr)
 plt.ylabel('Wins')
-z = np.polyfit(data_comb[x_attr],data_comb_df['W'],1)
+z = np.polyfit(data_comb[x_attr],data_comb['W'],1)
 p = np.poly1d(z)
 plt.plot(data_comb[x_attr],p(data_comb[x_attr]), "g--")
 plt.show()
-
-
-
-
-
-
 
