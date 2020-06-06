7ee2928c180568748893586905bdcd47aa64a46a
Devin Dhaliwal <devindha@usc.edu>
2020-06-06 13:14:16 -0700

    improved dataset + added new team stat viz feature
    - eliminated old dataset for hitting stats and now calculating them using the other dataset in order to include more years
    - added feature where user is able to see how a certain stat impacted a chosen team's wins over a certain number of years

--- baseball_predictions.py
+++ baseball_predictions.py
@@ -7,27 +7,30 @@ import matplotlib.pyplot as plt
 import pickle
 import seaborn as sns
 
-#getting hitting avgs data
-data = pd.read_csv("baseball.csv")
-data = data.filter(items=['Team','Year','W','OBP','SLG','BA'])
+#getting data
+data = pd.read_csv("teams.csv")
+data = data.filter(items=['franchID','yearID','AB','R','H','2B','3B','HR','BB','SO','SB','CS','W','RA','ER','ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP'])
+data = data.query('yearID >= 1916')
+data = data.rename(columns={'franchID':'Team','yearID':'Year'})
 
-#adding OPS
-ops = pd.DataFrame(data['OBP']+data['SLG'])
-data = data.join(ops)
-data.rename(columns={ data.columns[6]: "OPS" }, inplace = True)
+#adding BA, OBP, SLG, OPS
+hitting = pd.DataFrame(round(data['H']/data['AB'], 3))
+hitting.rename(columns={hitting.columns[0]: "BA"}, inplace=True)
+obp = pd.DataFrame(round((data['H']+data['BB'])/data['AB'], 3))
+hitting = hitting.join(obp)
+hitting.rename(columns={hitting.columns[1]: "OBP"}, inplace=True)
+slg = pd.DataFrame(round((data['H']+data['2B']+2*data['3B']+3*data['HR'])/data['AB'], 3))
+hitting = hitting.join(slg)
+hitting.rename(columns={hitting.columns[2]: "SLG"}, inplace=True)
+ops = pd.DataFrame(round(hitting['OBP']+hitting['SLG'], 3))
+hitting = hitting.join(ops)
+hitting.rename(columns={hitting.columns[3]: "OPS"}, inplace=True)
+data = data.join(hitting)
+data = data.dropna()
 
-#getting other hitting and pitching data
-data_1 = pd.read_csv("teams.csv")
-data_1 = data_1.filter(items=['teamID','yearID','R','H','2B','3B','HR','BB','SO','SB','CS','W','RA','ER','ERA','CG','SHO','SV','HA','HRA','BBA','SOA','E','DP','FP'])
-data_1 = data_1.query('yearID >= 1962')
-data_1 = data_1.rename(columns={'teamID':'Team','yearID':'Year'})
-
-#combining datasets
-data_comb = pd.merge(data,data_1,on=['Team','Year'])
-data_comb = data_comb.rename(columns={'W_x':'W'})
-
-#dropping uneeded data after combining
-data_comb = data_comb.drop(['Team','Year','W_y'],1)
+#getting list of teams and df with no team and year col
+team_names = data.Team.unique()
+data_comb = data.drop(['Team','Year'],1)
 
 #assigning columns to x and y for predictions
 x = data_comb.drop('W', 1)
@@ -58,7 +61,7 @@ def predict_wins():
 
     #getting user inputs for stats to predict wins
     while 1:
-        stat_choice = input("Please enter a stat you would like to test or \"done\" to continue: ")
+        stat_choice = input("Please enter a stat you would like to test or \"done\" to continue: ").upper()
         if stat_choice.lower() == "done":
             break
         else:
@@ -87,16 +90,55 @@ def predict_wins():
 #visualizing impact of user chosen stat on wins
 def visualize_stat():
     while 1:
-        stat_choice = input("Please enter a stat you would like visualize or \"done\" to quit: ")
+        stat_choice = input("Please enter a stat you would like visualize or \"done\" to return to main menu: ").upper()
         if stat_choice.lower() == "done":
             break
         else:
             if stat_choice in data_comb:
+                sns.set()
                 sns.regplot(x=stat_choice, y='W', data=data_comb, line_kws={'color': 'red'})
                 plt.show()
             else:
                 print("Invalid Stat...")
                 continue
+
+#visualizing impact of user chosen stat on a specific team's wins
+def visualize_team_stat():
+
+    while 1:
+        stat_choice = input("Please enter a stat you would like visualize or \"done\" to return to main menu: ").upper()
+        if stat_choice.lower() == "done":
+            break
+        else:
+            if stat_choice in data_comb:
+                team_choice = input("Please enter a team (3 letter abbreviation) you would like visualize " + stat_choice + " for: ").upper()
+                if team_choice in team_names:
+                        year_choice = int(input("Please enter a year you would like to start at (between 1916 and 2014): "))
+                        if year_choice >= 1916 and year_choice <= 2014:
+                            data_team = data[(data['Year'] > year_choice) & (data['Team'] == team_choice)]
+                            sns.set()
+                            fig = plt.figure(figsize=(15,10))
+                            ax = sns.lineplot(x='Year', y=stat_choice, data=data_team)
+                            ax2 = ax.twinx()
+                            sns.lineplot(x='Year', y='W', data=data_team, ax=ax2, color='r')
+                            ax.tick_params(axis='y', labelcolor='b')
+                            ax.yaxis.label.set_color('b')
+                            ax2.tick_params(axis='y', labelcolor='r')
+                            ax2.yaxis.label.set_color('r')
+                            ax.set_title(team_choice)
+                            fig.legend(labels=[stat_choice, 'Wins'])
+                            plt.show()
+                        else:
+                            print("Invalid Year...")
+                            continue
+                else:
+                    print("Invalid Team...")
+                    continue
+            else:
+                print("Invalid Stat...")
+                continue
+
+
 
 #printing menu including available stats and options to predict or graph
 def print_menu():
@@ -107,17 +149,20 @@ def print_menu():
     while 1:
         choice = input("Enter \"1\" to predict wins based on stats you enter"
                            "\nEnter \"2\" to visualize a stats impact on wins"
+                            "\nEnter \"3\" to visualize a stats impact on a specific team's wins over time"
                            "\nEnter \"quit\" to quit: ")
 
-        if choice != "1" and choice != "2" and choice.lower() != "quit":
+        if choice != "1" and choice != "2" and choice != "3" and choice.lower() != "quit":
             print("Invalid Input...")
             continue
         elif choice.lower() == "quit":
             return -1
         elif choice == "1":
             return 1
-        else:
+        elif choice == "2":
             return 2
+        else:
+            return 3
 
 def main():
 
@@ -130,6 +175,8 @@ def main():
             predict_wins()
         elif choice == 2:
             visualize_stat()
+        elif choice == 3:
+            visualize_team_stat()
         else:
             break
 
